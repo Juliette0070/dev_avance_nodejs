af6c615263359875389fb9e36203d7b0a126c42e
Rousselet Juliette <juliette.rousselet@etu.univ-orleans.fr>
2025-01-20 15:52:14 +0100

    refactor: replace http server with express framework

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const http = require('http');
+const express = require('express');
+const app = express();
+const PORT = 3000;
 
-http.createServer((req,res) => {
-    res.end('Hello World');
-}).listen(3000, () => {
-    console.log('Server is running on http://localhost:3000');
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
 });
